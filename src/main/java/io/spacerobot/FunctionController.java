90da6f83bf821ccb983d3a1fd61be6e57a37b6e3
Akseli Koskinen <koskinen.akseli@gmail.com>
2016-10-07 22:01:12 +0300

    Add command test to FunctionController
    (and delete some gradle files from repo)

--- src/main/java/io/spacerobot/FunctionController.java
+++ src/main/java/io/spacerobot/FunctionController.java
@@ -1,5 +1,7 @@
 package io.spacerobot;
 
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,16 @@ public class FunctionController {
 	
 	@RequestMapping("/function")
 	public Function function(@RequestParam(value="id") int id) {
+		
+		// Run command with id 'id'
+		Runtime rt = Runtime.getRuntime();
+		try {
+			Process pr = rt.exec("touch /tmp/thisfilewascreatedbyjava");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return new Function(9001);
 	}
 }
