efc8826828cfeb71d80ffcfe189aedf9014a957f
Akseli Koskinen <koskinen.akseli@gmail.com>
2016-10-07 22:09:27 +0300

    Create UserConfiguration class

---
+++ src/main/java/io/spacerobot/UserConfiguration.java
@@ -0,0 +1,31 @@
+package io.spacerobot;
+
+public class UserConfiguration {
+	private int port;
+	private String command1;
+	private String command2;
+	
+	public int getPort() {
+		return port;
+	}
+	
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	public String getCommand1() {
+		return command1;
+	}
+	
+	public void setCommand1(String command1) {
+		this.command1 = command1;
+	}
+	
+	public String getCommand2() {
+		return command2;
+	}
+	
+	public void setCommand2(String command2) {
+		this.command2 = command2;
+	}
+}
